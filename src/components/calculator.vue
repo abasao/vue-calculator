c0c46dbc2d045e5f969078b4a19eda2f875dd625
abasao <omar.abas.89@gmail.com>
2018-03-11 23:29:27 +0100

    first commit, setting up basic app

---
+++ src/components/calculator.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="calculator">
+    <h1>{{title}}</h1>
+    <form @submit="addNumber">
+      <input type="number" v-model="newNumber.numericValue" placeholder="NUMBER">
+      <br>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'calculator',
+    data() {
+      return {
+          title: 'Hello World',
+          newNumber: {},
+          numbers: [
+          ]
+      }
+    },
+    methods: {
+        addNumber(e){
+            e.preventDefault();
+            this.numbers.push(this.newNumber);
+            this.newNumber = {};
+            console.log(this.numbers);
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
+
